eb90d024edfe17ea341a3782221ac7b1efdfd417
pasta-pasta <122100177+pasta-pasta@users.noreply.github.com>
2023-07-01 02:29:46 +0700

    updated code
    / / for italic, & & for bold

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import textwrap
-
-import numpy as np
 from PIL import Image, ImageDraw, ImageFont
 
 
@@ -12,6 +9,7 @@ back = (255, 255, 255)
 base_image = Image.new("RGB", (w, h), back)
 draw = ImageDraw.Draw(base_image)
 font = ImageFont.truetype("timesbd.ttf", 35)
+
 text = str(task)
 textw, texth = draw.textsize(text, font=font)
 
@@ -31,6 +29,8 @@ draw.text((x, y), text, font=font, fill="black")
 
 # Пишем текст
 font = ImageFont.truetype("times.ttf", 30)
+fontbold = ImageFont.truetype("timesbd.ttf", 30)
+fontit = ImageFont.truetype("timesi.ttf", 30)
 text = open("text.txt", encoding="UTF-8").read()
 pad_left = rect_x1 + 20
 pad_right = pad_left / 2
@@ -39,36 +39,58 @@ max_width = w - (pad_right + pad_left)
 text_width = draw.textsize('A', font=font)[0]  # Ширина одной буквы
 chars_per_line = int(max_width // text_width)  # Ширина строки в символах
 
+fonts = {"st": font, "bd": fontbold, "it": fontit}
 
 currentstroka = ""
 currenth, line_spacing = 40, 5
 x = pad_left
+isbold = False
+isitalic = False
+curfont = "st"
 for i in text:
-    if draw.textsize(currentstroka, font=font)[0] >= max_width or i == "\n":
+    if i == "/":
+        if isitalic == False:
+            isitalic = True
+            curfont = "it"
+            continue
+        else:
+            isitalic = False
+            curfont = "st"
+            continue
+    elif i == "&":
+        if isbold == False:
+            isbold = True
+            curfont = "bd"
+            continue
+        else:
+            isbold = False
+            curfont = "st"
+            continue
+    if draw.textsize(currentstroka, font=fonts[curfont])[0] >= max_width or i == "\n":
         if i == " " or i == "\n":
             currenth += 33 + line_spacing
             currentstroka = ""
             x = pad_left
             continue
         elif i == "," or i == ".":
-            draw.text((x, y), i, font=font, fill="black")
+            draw.text((x, y), i, font=fonts[curfont], fill="black")
             currenth += 33 + line_spacing
             currentstroka = ""
-            x = pad_left - draw.textsize(i, font=font)[0]
+            x = pad_left - draw.textsize(i, font=fonts[curfont])[0]
             continue
         else:
             currenth += 33 + line_spacing
             currentstroka = ""
-            draw.text((x, y), "-", font=font, fill="black")
+            draw.text((x, y), "-", font=fonts[curfont], fill="black")
             x = pad_left
     y = currenth
-    draw.text((x, y), i, font=font, fill='black')
+    draw.text((x, y), i, font=fonts[curfont], fill='black')
     currentstroka += i
-    x+= draw.textsize(i, font=font)[0]
+    x+= draw.textsize(i, font=fonts[curfont])[0]
 
 lasttext = "Ответ: _____________________________."
 
-draw.text((pad_left, currenth+(h-currenth)/2), lasttext, font=font, fill="black")
+draw.text((pad_left, currenth+min((h-currenth)/2, 100)), lasttext, font=font, fill="black")
 
 
 base_image.save("output.png")
